bec449925c8b557ef18413eec0e0d4c4f8477ba2
Jayden Thadani <jayden.thad13@gmail.com>
2024-07-31 17:58:45 +0530

    math-82, code | add derivative table to EulersMethod

--- Differential Equations --- math-82/code/EulersMethod/EulersMethod.py
+++ Differential Equations --- math-82/code/EulersMethod/EulersMethod.py
@@ -3,10 +3,9 @@ import math
 def EulersMethod(x_0, t_0, t_f, h, xdot):
     x = [x_0]
     t = [t_0]
+    xdots = []
     while (t[-1] < t_f):
-        x.append(xdot(x[-1], t[-1]) * h + x[-1])
+        xdots.append(xdot(x[-1], t[-1]))
+        x.append(xdots[-1] * h + x[-1])
         t.append(t[-1] + h)
-    return x
-
-if __name__ == "__main__":
-    print(EulerMethod(x_0, t_0, t_f, h, xdot))
+    return x, xdots
