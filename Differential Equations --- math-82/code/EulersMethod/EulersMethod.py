3339ef4034bc6a82aa2200edf540eca4fa11d36a
Jayden Thadani <jayden.thad13@gmail.com>
2024-07-31 18:40:21 +0530

    math-82, code | fix: make EulersMethod output human-readable

--- Differential Equations --- math-82/code/EulersMethod/EulersMethod.py
+++ Differential Equations --- math-82/code/EulersMethod/EulersMethod.py
@@ -8,4 +8,17 @@ def EulersMethod(x_0, t_0, t_f, h, xdot):
         xdots.append(xdot(x[-1], t[-1]))
         x.append(xdots[-1] * h + x[-1])
         t.append(t[-1] + h)
-    return x, xdots
+    return x, xdots, t
+
+def read(ar):
+    l = len(ar[0]) - 1
+    print("t \tx \tx'")
+    for i in range(l):
+        print(str(round(ar[2][i], 5)) + "\t" + str(round(ar[0][i], 5)) + "\t" + str(round(ar[1][i], 5)))
+    print(str(round(ar[2][l], 5)) + "\t" + str(round(ar[0][l], 5)))
+
+def ydot(y, t):
+    return 1 - t + y # enter formula for y' here
+
+if __name__ == "__main__":
+    read(EulersMethod(1, 0, 1, 0.1, ydot)) # enter initial conditions here
